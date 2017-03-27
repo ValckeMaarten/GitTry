b8dc76df4f03bf66133c27b2663780b03f2a2f85
ValckeMaarten <maarten.valcke@student.howest.be>
2017-03-27 14:21:19 +0200

    added Sudoku class

---
+++ src/gitgud/SuDoKu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitgud;
+
+/**
+ *
+ * @author maart
+ */
+public class SuDoKu {
+    
+    public void SuDoKu(){
+        
+    }
+    
+    public boolean isSolvable(){
+        return true;
+    }
+}
